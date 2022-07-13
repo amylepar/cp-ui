1db20a17979d01be1f697d672ab55f6109b686b3
amylepar <amylepar@gmail.com>
2022-07-13 11:33:47 +0300

    Include the filters and the infinite scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import './App.css';
 import { CounterParty } from './api/cp-api/Counterparty';
-import { ColDef, GridReadyEvent, ICellRendererParams, IDatasource } from 'ag-grid-community';
+import { ColDef, GetRowIdParams, GridReadyEvent, ICellRendererParams, IDatasource } from 'ag-grid-community';
 
 
 function App() {
@@ -14,8 +14,7 @@ function App() {
   
   const [columnDefs] = useState([
       // example from https://www.ag-grid.com/react-data-grid/infinite-scrolling/
-      { header: 'LEI', 
-        field: 'lei', 
+      { header: 'ID',         
         valueGetter: 'node.id', 
         cellRenderer: (props: ICellRendererParams) => {
         if (props.value !== undefined) {
@@ -25,13 +24,18 @@ function App() {
             <img src="loading.gif" />
           );
         }
-      }, },
-      { header: 'Legal Name', field: 'legalName'},      
-      { header: 'Country', field: 'legalAddress.country' },
-      { header: 'City', field: 'legalAddress.city' },
-      { header: 'Entity Category', field: 'entityCategory' },
-      { header: 'Registration Date', field: 'registrationDetails.initialRegistrationDate' },
-      { header: 'Renewal Date', field: 'registrationDetails.nextRenewalDate' }
+      },
+      sortable: false,
+      filter: false,
+      suppressMenu: true
+    }, 
+    { header: 'LEI', field: 'lei'},
+    { header: 'Legal Name', field: 'legalName'},      
+    { header: 'Country', field: 'legalAddress.country' },
+    { header: 'City', field: 'legalAddress.city' },
+    { header: 'Entity Category', field: 'entityCategory' },
+    { header: 'Registration Date', field: 'registrationDetails.initialRegistrationDate' },
+    { header: 'Renewal Date', field: 'registrationDetails.nextRenewalDate' }
   ]);
 
   const defaultColDef = useMemo<ColDef>(() => {
@@ -39,41 +43,112 @@ function App() {
       editable: true,
       resizable: true,
       minWidth: 100,
-      flex: 1,
+      flex: 1,      
+      sortable: true,
       filter: true, 
-      sortable: true 
+      floatingFilter: true
     };
   }, []);
 
+  const sortAndFilter = (
+    allOfTheData: any[],
+    sortModel: any,
+    filterModel: any
+  ) => {
+    return sortData(sortModel, filterData(filterModel, allOfTheData));
+  };
+  
+  const sortData = (sortModel: any, data: any[]) => {
+    const sortPresent = sortModel && sortModel.length > 0;
+    if (!sortPresent) {
+      return data;
+    }
+    // do an in memory sort of the data, across all the fields
+    const resultOfSort = data.slice();
+    resultOfSort.sort(function (a, b) {
+      for (let k = 0; k < sortModel.length; k++) {
+        const sortColModel = sortModel[k];
+        const valueA = a[sortColModel.colId];
+        const valueB = b[sortColModel.colId];
+        // this filter didn't find a difference, move onto the next one
+        if (valueA == valueB) {
+          continue;
+        }
+        const sortDirection = sortColModel.sort === 'asc' ? 1 : -1;
+        if (valueA > valueB) {
+          return sortDirection;
+        } else {
+          return sortDirection * -1;
+        }
+      }
+      // no filters found a difference
+      return 0;
+    });
+    return resultOfSort;
+  };
+  
+  const filterData = (filterModel: any, data: any[]) => {
+    const filterPresent = filterModel && Object.keys(filterModel).length > 0;
+    if (!filterPresent) {
+      return data;
+    }
+    const resultOfFilter = [];
+    for (let i = 0; i < data.length; i++) {
+      const item = data[i];    
+      if (filterModel.lei) {
+        if (filterModel.lei.values.indexOf(item.lei) < 0) {
+          continue;
+        }
+      }
+      resultOfFilter.push(item);
+    }
+    return resultOfFilter;
+  };
+
+  const getRowId = useCallback(function (params: GetRowIdParams) {
+    return params.data.id;
+  }, []);
+  
   const onGridReady = useCallback((params: GridReadyEvent) => {
     fetch('json/cptestdata.json')
     .then(result => result.json())
-    .then(data => 
-      {
-        const dataSource: IDatasource = {
-          rowCount: undefined,
-          getRows: (params) => {
-            console.log(
-              'asking for ' + params.startRow + ' to ' + params.endRow
+    .then((data: any[]) => {
+      // give each row an id
+      data.forEach(function (d, index) {
+        d.id = 'R' + (index + 1);
+      });
+      const dataSource: IDatasource = {
+        rowCount: undefined,
+        getRows: (params) => {
+          console.log(
+            'asking for ' + params.startRow + ' to ' + params.endRow
+          );
+          // At this point in your code, you would call the server.
+          // To make the demo look real, wait for 500ms before returning
+          setTimeout(function () {
+            // take a slice of the total rows
+            const dataAfterSortingAndFiltering = sortAndFilter(
+              data,
+              params.sortModel,
+              params.filterModel
             );
-            // At this point in your code, you would call the server.
-            // To make the demo look real, wait for 500ms before returning
-            setTimeout(function () {
-              // take a slice of the total rows
-              const rowsThisPage = data.slice(params.startRow, params.endRow);
-              // if on or after the last page, work out the last row.
-              let lastRow = -1;
-              if (data.length <= params.endRow) {
-                lastRow = data.length;
-              }
-              // call the success callback
-              params.successCallback(rowsThisPage, lastRow);
-            }, 500);
-          },
-        };
-        params.api.setDatasource(dataSource);
-      })
-  }, []);
+            const rowsThisPage = dataAfterSortingAndFiltering.slice(
+              params.startRow,
+              params.endRow
+            );
+            // if on or after the last page, work out the last row.
+            let lastRow = -1;
+            if (dataAfterSortingAndFiltering.length <= params.endRow) {
+              lastRow = dataAfterSortingAndFiltering.length;
+            }
+            // call the success callback
+            params.successCallback(rowsThisPage, lastRow);
+          }, 500);
+        },
+      };
+      params.api.setDatasource(dataSource);
+    });
+}, []);
 
   const onBtExport = useCallback(() => {
     gridRef.current!.api.exportDataAsExcel();
